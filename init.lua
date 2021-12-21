61d0b60d74af1e285698a6eb73ed0f10bc7be6af
The_philosopher <shinudonney@tuta.io>
2021-12-21 14:48:54 +0530

    Clean slate reset

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
----||  @init.lua  ||---
-
-require('core.keymaps')
-require('core.options')
-require('plugins')
-require('plugins.configs.icons')
-require('plugins.configs.nvimtree')
-require('plugins.configs.lspconfig')
-require('plugins.configs.telescope')
+-- @init.lua --
