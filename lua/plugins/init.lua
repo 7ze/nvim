ae418c088beed1e9864ff02bd9aad3f8d4f504a8
The_philosopher <shinudonney@tuta.io>
2021-12-22 18:07:14 +0530

    Set up packer

---
+++ lua/plugins/init.lua
@@ -0,0 +1,64 @@
+-- @plugins/init.lua --
+
+-- bootstrapping
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer..."
+  vim.cmd [[ packadd packer.nvim ]]
+end
+
+-- autocmd to packer sync whenever you save this file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost lua/plugins/init.lua source <afile> | PackerSync
+  augroup end
+]]
+
+local status_ok, packer = pcall(require, "packer")
+-- exits if packer cannot be invoked
+if not status_ok then
+  vim.notify "Packer could not be loaded!"
+  return
+end
+
+-- custom initialization
+packer.init {
+  display = {
+    open_fn = function() return require("packer.util").float { border = "rounded" } end,  -- An optional function to open a floating window for packer
+    working_sym = '🛠', -- The symbol for a plugin being installed/updated
+    error_sym = '🧨', -- The symbol for a plugin with an error in installation/updating
+    done_sym = '🎉', -- The symbol for a plugin which has completed installation/updating
+    removed_sym = '🔥', -- The symbol for an unused plugin which was removed
+    moved_sym = '🚀', -- The symbol for a plugin which was moved (e.g. from opt to start)
+    header_sym = '━', -- The symbol for the header line in packer's display
+    show_all_info = true, -- Should packer show all update details automatically?
+    --prompt_border = 'double', -- Border style of prompt popups.
+  }
+}
+
+local use = packer.use
+packer.reset()
+
+packer.startup(function()
+  print "From packer startup()"
+
+  -- plugins
+  use "wbthomason/packer.nvim"
+
+  -- automatically sets up plugins after bootstrap
+  if PACKER_BOOTSTRAP then
+    packer.sync()
+  end
+end)
+
