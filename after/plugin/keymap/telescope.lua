ef63b45c505a3f55e609f992f75558bcc8e9a3f6
The_philosopher <email.shinudonney@gmail.com>
2022-10-30 10:36:50 +0530

    let find_files picker show hidden files telescope

--- after/plugin/keymap/telescope.lua
+++ after/plugin/keymap/telescope.lua
@@ -1,7 +1,7 @@
 local Remap = require("zeus.map")
 local nnoremap = Remap.nnoremap
 
-nnoremap("<leader>ff", ":Telescope find_files<cr>")
+nnoremap("<leader>ff", ":Telescope find_files hidden=true<cr>")
 nnoremap("<leader>fg", ":Telescope live_grep<cr>")
 nnoremap("<leader>fb", ":Telescope file_browser<cr>")
 nnoremap("<leader>fh", ":Telescope help_tags<cr>")
