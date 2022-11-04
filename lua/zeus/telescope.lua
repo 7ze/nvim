31ca7d3ea8c4d9864c1bf8ec5b5c3d3c636467c0
The_philosopher <email.shinudonney@gmail.com>
2022-11-04 19:45:40 +0530

    find_files support for hidden files - telescope (change hidden files setting to telescope setup file)

--- lua/zeus/telescope.lua
+++ lua/zeus/telescope.lua
@@ -44,6 +44,9 @@ require("telescope").setup {
         },
     },
     pickers = {
+        find_files = {
+            hidden = true
+        },
         live_grep = {
             additional_args = function()
                 return { "--hidden" }
